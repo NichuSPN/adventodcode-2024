fcb99fd77a03fa93b24297e28bba3062650df936
Nishaanth S P <nishaanth.sp@swymcorp.com>
2024-12-07 14:11:30 +0530

    All answers

---
+++ day07/part1.py
@@ -0,0 +1,45 @@
+import itertools
+import tqdm
+
+# INPUT_FILE_NAME = 'example.txt'
+INPUT_FILE_NAME = 'raminput.txt'
+
+input = {}
+with open(f'input/{INPUT_FILE_NAME}', 'r') as file:
+    # Read each line in the file
+    for line in file:
+        # Print each line
+        [k, v] = line.strip().split(': ')
+        input[k]=[int(x) for x in v.split(' ')]
+        
+def add(x, y): 
+    return x+y
+
+def mul(x, y):
+    return x*y
+
+combMap = {}
+
+def getCombinations(repeat):
+    if str(repeat) in combMap:
+        return combMap[str(repeat)]
+    combs = list(itertools.product([add, mul], repeat=repeat))
+    combMap[str(repeat)] = combs
+    return combs
+
+def anyEquationPresent(testValue, numbers):
+    n = len(numbers)
+    combinations = getCombinations(n-1)
+    for comb in combinations:
+        evaluated = numbers[0]
+        for i in range(n-1):
+            evaluated = comb[i](evaluated, numbers[i+1])
+        if evaluated==int(testValue):
+            return evaluated 
+    return 0
+
+totalOfValidValues = 0
+for testValue, numbers in tqdm.tqdm(input.items()):
+    totalOfValidValues+=anyEquationPresent(testValue, numbers)
+    
+print(f"ta-da... Here is the sum of valid test values: {totalOfValidValues}")
---
+++ day07/part2.py
@@ -0,0 +1,81 @@
+import itertools
+import tqdm
+
+# INPUT_FILE_NAME = 'example.txt'
+INPUT_FILE_NAME = 'actual.txt'
+
+input = {}
+with open(f'input/{INPUT_FILE_NAME}', 'r') as file:
+    # Read each line in the file
+    for line in file:
+        # Print each line
+        [k, v] = line.strip().split(': ')
+        input[k]=v.split(' ')
+        
+combMap = {}
+        
+def getCombinations(ops, repeat, concatReq=False):
+    searchStr = str(repeat)
+    if concatReq:
+        searchStr+='|'
+    if searchStr in combMap:
+        return combMap[searchStr]
+    combs = list(itertools.product(ops, repeat=repeat))
+    if concatReq:
+        combs = [comb for comb in combs if '|' in comb]
+    combMap[searchStr] = combs
+    return combs
+        
+def evalOp (x, op, y):
+    if op=='+':
+        return str(int(x)+int(y)) 
+    if op=='*':
+        return str(int(x)*int(y)) 
+    if op=='|':
+        return x+y
+    
+def combineNumbersWithOps(numbers, opsComb):
+    n = len(numbers)
+    eq = numbers[:1]
+    for i in range(n-1):
+        eq.append(opsComb[i])
+        eq.append(numbers[i+1])
+    return eq
+
+evalMemo = {}
+
+def evaluateEquation(eq):
+    searchStr = ''.join(eq)
+    if searchStr in evalMemo:
+        return evalMemo[searchStr]
+    if len(eq)==3:
+        result = evalOp(*eq)
+        evalMemo[searchStr] = result
+        return result
+    result = evalOp(evaluateEquation(eq[:-2]), eq[-2], eq[-1])
+    evalMemo[searchStr] = result
+    return result
+
+def anyEquationPassed(testValue, numbers, combinations):
+    for comb in combinations:
+        eq = combineNumbersWithOps(numbers, comb)
+        if evaluateEquation(eq)==testValue:
+            return int(testValue)
+    return 0
+
+validInIteration1 = []
+for testValue, numbers in tqdm.tqdm(input.items()):
+    combinations = getCombinations(['+', '*'], len(numbers)-1)
+    if anyEquationPassed(testValue, numbers, combinations) > 0:
+        validInIteration1.append(testValue)
+
+evalMemo = {}
+totalOfOtherValidValues = 0
+for testValue, numbers in tqdm.tqdm(input.items()):
+    if testValue in validInIteration1:
+        continue
+    combinations = getCombinations(['+', '*', '|'], len(numbers)-1, True)
+    totalOfOtherValidValues+=anyEquationPassed(testValue, numbers, combinations)
+
+finalResult = sum([int(x) for x in validInIteration1])+totalOfOtherValidValues
+print(f"ta-da... Here is the sum of valid test values: {finalResult}")
